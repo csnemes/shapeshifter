b7eb98daf8eb9bbe078cbf78448fa818bf5169c2
realvizu <vizkeletiferenc@gmail.com>
2014-05-22 09:57:45 +0200

    Added tests for experimenting

---
+++ Shapeshifter.Tests.Unit/New/CustomSerializerTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Shapeshifter.Core;
+using Shapeshifter.Tests.Unit.RoundtripTests;
+
+namespace Shapeshifter.Tests.Unit.New
+{
+    [TestFixture]
+    public class CustomSerializerTests:TestsBase
+    {
+        [Test]
+        public void CustomerSerializerForConcreteType_Success()
+        {
+            var source = new MyType() { MyProperty = 42 };
+            
+            var serializer = GetSerializer<MyType>();
+            var pack = serializer.Serialize(source);
+            var jobj = JObject.Parse(pack);
+
+            jobj[Constants.TypeNameKey].Value<string>().Should().Be("MyType");
+            jobj[Constants.VersionKey].Value<uint>().Should().Be(1);
+            jobj["MyKey"].Value<int>().Should().Be(42);
+        }
+
+        [Test]
+        public void CustomerSerializerForUnboundGenericType_Success()
+        {
+            var source = new MyType<string>() { MyProperty = 42 };
+
+            var serializer = GetSerializer<MyType<string>>();
+            var pack = serializer.Serialize(source);
+            var jobj = JObject.Parse(pack);
+
+            jobj[Constants.TypeNameKey].Value<string>().Should().Be("MyType<string>");
+            jobj[Constants.VersionKey].Value<uint>().Should().Be(1);
+            jobj["UnboundGenericType.MyKey"].Value<int>().Should().Be(42);
+        }
+
+        [Test]
+        public void CustomerSerializerForBoundGenericType_Success()
+        {
+            var source = new MyType<int>() { MyProperty = 42 };
+
+            var serializer = GetSerializer<MyType<int>>();
+            var pack = serializer.Serialize(source);
+            var jobj = JObject.Parse(pack);
+
+            jobj[Constants.TypeNameKey].Value<string>().Should().Be("MyType<int>");
+            jobj[Constants.VersionKey].Value<uint>().Should().Be(1);
+            jobj["BoundGenericType.MyKey"].Value<int>().Should().Be(42);
+        }
+
+        [Serializer]
+        public class MyType
+        {
+            public int MyProperty { get; set; }
+
+            // Minek a verziószám, miért nem generál egyet?
+            [Serializer(typeof (MyType), 1)]
+            public static void Serializer(IPackformatValueWriter writer, MyType itemToSerialize)
+            {
+                writer.SetValue("MyKey", itemToSerialize.MyProperty);
+            }
+        }
+
+        [Serializer]
+        public class MyType<T>
+        {
+            public int MyProperty { get; set; }
+
+            [Serializer(typeof(MyType<>), 1)]
+            public static void UnboundGenericTypeSerializer(IPackformatValueWriter writer, MyType itemToSerialize)
+            {
+                writer.SetValue("UnboundGenericType.MyKey", itemToSerialize.MyProperty);
+            }
+
+            [Serializer(typeof(MyType<int>), 1)]
+            public static void BoundGenericTypeSerializer(IPackformatValueWriter writer, MyType itemToSerialize)
+            {
+                writer.SetValue("BoundGenericType.MyKey", itemToSerialize.MyProperty);
+            }
+        }
+    }
+}
---
+++ Shapeshifter.Tests.Unit/New/DefaultRoundtripTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using Shapeshifter.Tests.Unit.RoundtripTests;
+
+namespace Shapeshifter.Tests.Unit.New
+{
+    [TestFixture]
+    public class DefaultRoundtripTests :TestsBase
+    {
+        [Test]
+        [ExpectedException(typeof(ShapeshifterException))]
+        public void TypeWithoutSerializerAttribute_Throws()
+        {
+            var source = new TypeWithoutSerializerAttribute();
+            var serializer = GetSerializer<TypeWithoutSerializerAttribute>();
+            serializer.Serialize(source);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ShapeshifterException))]
+        public void TypeWithoutDataContractAttribute_Throws()
+        {
+            var source = new TypeWithoutDataContractAttribute();
+            var serializer = GetSerializer<TypeWithoutDataContractAttribute>();
+            serializer.Serialize(source);
+        }
+
+        [Test]
+        public void TypeWithSerializerAndDataContractAttribute_SuccessfulRoundtrip()
+        {
+            var source = new TypeWithSerializerAndDataContractAttribute() {MyProperty = 42};
+            var serializer = GetSerializer<TypeWithSerializerAndDataContractAttribute>();
+            var pack = serializer.Serialize(source);
+            var target = serializer.Deserialize(pack);
+            target.MyProperty.Should().Be(42);
+        }
+
+        [Serializer]
+        public class TypeWithoutDataContractAttribute
+        {
+            public int MyProperty { get; set; }
+        }
+
+        [DataContract]
+        public class TypeWithoutSerializerAttribute
+        {
+            [DataMember]
+            public int MyProperty { get; set; }
+        }
+
+        [Serializer]
+        [DataContract]
+        public class TypeWithSerializerAndDataContractAttribute
+        {
+            [DataMember]
+            public int MyProperty { get; set; }
+        }
+    }
+}
